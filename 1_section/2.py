2ecf823a58347e1732bb32b884195c8b6ff4bcd1
FumihisaKobayashi <ruruku717@gmial.com>
2020-07-26 00:14:16 +0900

    確認 python 関数・変数

---
+++ 1_section/2.py
@@ -0,0 +1,16 @@
+#pythonの基礎
+
+#=は代入
+value = 5
+char = "Test"
+train = pd.read_csv("train.csv")
+
+#変数の中の変数
+train.shape
+
+#関数
+train.plot(option)
+#,で区切ると複数
+train.plot(option, option2)
+
+#スペルミス、大文字、小文字、（）の種類に気をつける
